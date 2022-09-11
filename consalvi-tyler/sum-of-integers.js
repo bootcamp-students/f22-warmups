99f2634792c4859e08e49ce5d8e9550927e66232
Tyler Consalvi <tkconsalvi@gmail.com>
2022-09-11 19:52:50 +0000

    2nd kata down

---
+++ consalvi-tyler/sum-of-integers.js
@@ -0,0 +1,15 @@
+/*
+FUNCTION getSum (a,b)
+sum = a + b
+    IF (a === b)
+        return a;
+    ELSE
+        return sum
+*/
+function getSum( a,b ) {
+    let sum = a + b;
+      if (a === b) 
+        return a;
+      else
+        return sum;
+  }
