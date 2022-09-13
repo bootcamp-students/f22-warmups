75efa3b73e49ee772404e0424ec5c84103d2a683
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-09-13 16:48:29 +0000

    rewritten code on kata

--- consalvi-tyler/sum-of-integers.js
+++ consalvi-tyler/sum-of-integers.js
@@ -1,15 +1,20 @@
 /*
 FUNCTION getSum (a,b)
-sum = a + b
-    IF (a === b)
-        return a;
-    ELSE
-        return sum
+create variables to find if a<b or a>b and place them 
+INIT sum
+FOR LOOP between MIN and MAX
+for each increment of loop add to sum
+
+IF a === b RETURN a
+ELSE return sum
 */
-function getSum( a,b ) {
-    let sum = a + b;
-      if (a === b) 
-        return a;
-      else
-        return sum;
+
+function getSum(a, b) {
+  let min = Math.min(a, b), max = Math.max(a, b);
+  let sum = 0;
+  for (let j = min; j <= max; j++) {
+  sum += j
   }
+  if (a === b) return a;
+  else return sum;
+}
