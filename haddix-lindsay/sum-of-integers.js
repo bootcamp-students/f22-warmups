9fabf1195b113dd99b596e67aaf140992d826898
Lynz951 <83032886+Lynz951@users.noreply.github.com>
2022-09-13 16:08:38 -0400

    Create sum-of-integers.js

---
+++ haddix-lindsay/sum-of-integers.js
@@ -0,0 +1,9 @@
+function GetSum(a, b) {
+    //find sum of a and b
+    //add integers between a and b
+    //return sum
+    return (a + b) * (Math.abs(a - b) + 1) / 2;
+  }
+  
+    //I found this hard, definitely looked at the answers. 
+    //I understand now but I don't know if I could have ever come up with that formula on my own.pi
