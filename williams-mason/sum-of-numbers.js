d09a45ecb4c176779f8d78600a4a7b054f00fe65
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-09-17 22:17:07 +0000

    adds comments and starts function

---
+++ williams-mason/sum-of-numbers.js
@@ -0,0 +1,15 @@
+/*
+Given two integers a and b, which can be positive or negative
+
+find the sum of all the integers between and including them
+ 
+and return it
+ 
+ If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+*/
+function getSum (a, b) {
+    
+}
