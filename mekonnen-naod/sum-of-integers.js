f4caffa505eba78de60802af07981708e1a78cf7
naodmekonnen <100172439+naodmekonnen@users.noreply.github.com>
2022-09-17 18:24:12 -0400

    Create sum-of-integers.js

---
+++ mekonnen-naod/sum-of-integers.js
@@ -0,0 +1,11 @@
+// LET min:the smallest value
+// LET max:the largest value
+// FUNCTION : sum of all integers b/n a and b (s=n(a+b)/2)
+
+
+function getSum( a,b ){
+  
+ let min = Math.min(a,b),
+     max = Math.max(a,b)
+ return ((max-min + 1) * (max + min))/2
+  }
