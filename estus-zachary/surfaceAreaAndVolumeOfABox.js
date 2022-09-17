0efa48595132635c5cc55606799d87f52551ba6d
Zach Estus <43345401+Zestus01@users.noreply.github.com>
2022-09-17 19:27:48 -0400

    This is ONE of the THREE

--- estus-zachary/surfaceAreaAndVolumeOfABox.js
+++ estus-zachary/surfaceAreaAndVolumeOfABox.js
@@ -2,7 +2,7 @@
 // Each side is 2 dimensions times each other
 // Value[0] is the area
 // Value[1] is the volume
-
+//
 function getSize(width, height, depth) {
   let value = [];
   value[0] = ((width * height) * 2) + ((width * depth) * 2) + ((height * depth) * 2);
