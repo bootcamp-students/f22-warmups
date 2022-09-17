b55609212fa07ee5a1ffa29631badafe51d79f9c
naodmekonnen <100172439+naodmekonnen@users.noreply.github.com>
2022-09-17 17:48:17 -0400

    Create area volume of box.js

---
+++ mekonnen-naod/area volume of box.js
@@ -0,0 +1,18 @@
+// GET length
+// GET width
+// GET height
+
+// FUNCTION:Calculate Area
+// FUNCTION:Calculate Volume
+
+// RETURN [Area, Volume]
+
+function getSize(width, height, depth){
+
+Area = 2*((depth*width) + (depth*height) + (width*height));
+
+Volume = depth * width * height;
+          
+Return [Area, Volume];
+
+}
