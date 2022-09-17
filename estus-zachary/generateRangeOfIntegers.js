3af51d05d032d65c29233b35f701faf0c3f82859
Zach Estus <43345401+Zestus01@users.noreply.github.com>
2022-09-17 19:00:03 -0400

    Create generateRangeOfIntegers.js

---
+++ estus-zachary/generateRangeOfIntegers.js
@@ -0,0 +1,14 @@
+// Create an array
+// While the min is less than the max add it to the array
+// Increase the min by the step
+
+function generateRange(min, max, step){
+  let range = [];
+  let index = 0;
+  while(min <= max){
+    range[index] = min;
+    index++;
+    min += step;
+  }
+  return range;
+}
