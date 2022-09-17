712069f229cce15eca1a19069dc80677cdeac11f
Zach Estus <43345401+Zestus01@users.noreply.github.com>
2022-09-17 18:45:42 -0400

    Added comments

--- estus-zachary/abbreviateATwoWordName.js
+++ estus-zachary/abbreviateATwoWordName.js
@@ -1,3 +1,9 @@
+// Declare string holder and get the first letter.
+// Uppercase the first letter.
+// Split the given string into two indexes by split
+// Get the second word in the string
+// Catanate the string together and make uppercase
+
 function abbrevName(name){
     let newName = name[0].toUpperCase() + '.';
     let second = name.split(' ');
