0e26f9d4fd9503190f12b549b11ee52b70d6f1d8
Zach Estus <43345401+Zestus01@users.noreply.github.com>
2022-09-17 19:03:02 -0400

    Create findMaximumandMinimumValuesOfAList.js

---
+++ estus-zachary/findMaximumandMinimumValuesOfAList.js
@@ -0,0 +1,11 @@
+// Math libary has built in functions to find the min and max
+
+var min = function(list){
+    let min = Math.min(...list);
+    return min;
+}
+
+var max = function(list){
+    let max = Math.max(...list);
+    return max;
+}
