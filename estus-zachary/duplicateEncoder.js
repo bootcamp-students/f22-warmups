f9a30ab37053f9d0ebc0d227921899c8f2131a41
Zach Estus <43345401+Zestus01@users.noreply.github.com>
2022-09-17 19:14:53 -0400

    Added comments

--- estus-zachary/duplicateEncoder.js
+++ estus-zachary/duplicateEncoder.js
@@ -1,3 +1,10 @@
+// // Create an empty string to return
+// set count to -1 to avoid off by one errors
+// Count to -1 allows comparision to itself, and gives correct value for similiar characters
+// Make the string lower case to avoid issues with that
+// Step throug the array and compare the value
+// Depending on the count Add ( or ) to the encoded string
+
 function duplicateEncode(word){
     let newStr = '';
     let count = -1;
