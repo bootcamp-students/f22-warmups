47c3e0112c41404baa76c626adfa2fdf44be3d22
Zach Estus <43345401+Zestus01@users.noreply.github.com>
2022-09-17 19:17:44 -0400

    Added comments

--- estus-zachary/vowelCount.js
+++ estus-zachary/vowelCount.js
@@ -1,9 +1,13 @@
-
+// Check each character of the string for vowel
+// Lower case the string to avoid checking capitals
+// increase the count of vowels by 1 when vowel
+// return count
 
 function getCount(str) {
   let count = 0;
+  str = str.toLowerCase()
   for(let char of str)
-    if(char === 'a' || char === 'A' || char === 'e' || char === 'E' || char === 'u' || char === 'U' || char === 'o' || char === 'O' || char === 'I' || char === 'i')
+    if(char === 'a' || char === 'e' || char === 'u' || char === 'o' || char === 'i')
       count++;
   return count;
 }
