8065b29668396b4e750fd1a0a0cad961b0b100aa
Zach Estus <43345401+Zestus01@users.noreply.github.com>
2022-09-17 19:39:18 -0400

    Create grasshopperPersonalizedMessage.js

---
+++ estus-zachary/grasshopperPersonalizedMessage.js
@@ -0,0 +1,8 @@
+// Check if name is the same as owner
+// Display correct message depending on the boolean
+function greet (name, owner) {
+  if(name === owner)
+    return "Hello boss";
+  else
+    return "Hello guest";
+}
