9996150fbde1c0a9b12d0100e01259d7e845cb4b
Jeff Goens <jeff.goens@student.awesomeinc.org>
2022-09-18 16:08:01 -0400

    Update sum-of-numbers.js

--- goens-jeff/sum-of-numbers.js
+++ goens-jeff/sum-of-numbers.js
@@ -2,26 +2,24 @@
 // Assumptions: number a and b can be positive or negative, and equal
 
 // START
-// 
-// RETURN Sum of a and b
+// DETERMINE: min and max of integers
+// CALCULATE: for loop to get integers
+// RETURN: sum of a and b
 // END
 
 
-
-let min = Math.min(a,b); //returns the lower between both args and puts it into min variable
-let max = Math.max(a,b); //returns the higher arg and stores it into max variable
-let sum = 0
-
-
 function getSum( a,b ) {
+  let min = Math.min(a,b); 
+  let max = Math.max(a,b); 
+  let sum = 0;
+    
   if (min === max) {
     return min;
   } else {
-    for ( i = min, i <= max, i++) {
-      sum = i;
+      for (let i = min; i <= max; i++) {
+        sum += i;
     }
   }
-  return sum
+  return sum;
 }
-
 
