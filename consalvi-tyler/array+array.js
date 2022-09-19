cbd39914add8674f85a2c20afa24e979258b3c59
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-09-19 22:39:25 +0000

    kaTa

---
+++ consalvi-tyler/array+array.js
@@ -0,0 +1,10 @@
+/* 
+get full sum each individual array
+FOR loop add each integer to the sum
+    sum1 = 0
+    for num of arr1 sum += nums(2)
+add full sums of each array
+    finalSum = arr1Sum + arr2Sum
+return a number
+    return finalSum
+*/
---
+++ consalvi-tyler/tip-caclulator.js
@@ -0,0 +1,33 @@
+/*
+Set each rating for * appropriate tip
+rating.toLowerCase
+INIT tip
+terrible = 0
+poor = 0.05
+good = 0.1
+great = 0.15
+excellent = 0.2
+
+return (tip * amount)
+*/
+
+function calculateTip(amount, rating) {
+
+  rating = rating.toLowerCase();
+  let tip = 0;
+  
+  if (rating === "terrible") {
+    tip = 0;
+    } else if (rating === "poor") {
+    tip = 0.05;
+    } else if (rating === "good") {
+    tip = 0.1;
+    } else if (rating === "great") {
+    tip = 0.15;
+    } else if (rating === "excellent") {
+    tip = 0.2;
+    } else {
+      return 'Rating not recognised'
+    }
+    return Math.ceil(tip * amount);
+}
