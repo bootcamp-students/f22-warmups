e51b52e3463dce9fe88cf70063acac5eb7a51799
elijahwenndt <100172446+elijahwenndt@users.noreply.github.com>
2022-09-18 21:02:54 -0400

    Create Surface Area-and-Volume-of-a-Box.js
    //get length
    //get width
    //get height
    //use area formula to calculate area
    //use volume formula to calculate volume

---
+++ wenndt-elijah/Surface Area-and-Volume-of-a-Box.js
@@ -0,0 +1,12 @@
+function getSize(width, height, depth) {
+//pseudocode
+//get length
+//get width
+//get height
+//use area formula to calculate area
+//use volume formula to calculate volume
+
+area = (2 * depth * width) + (2 * depth * height) + (2 * width * height);
+volume = (depth * width * height);
+return [area, volume];
+  }
