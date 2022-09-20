a84007486bbd76f90d8306ff1f7dde1e62bb7f05
Jeff Goens <jeff.goens@student.awesomeinc.org>
2022-09-20 07:31:17 -0400

    Create tip-calculator.js
    Kata 9/19

---
+++ goens-jeff/tip-calculator.js
@@ -0,0 +1,40 @@
+// Pseudocode
+
+// START
+// CREATE tip variable
+
+function calculateTip(amount, rating) {
+  
+  let tip = 0;
+  rating = rating.toLowerCase ();
+  
+// CREATE if statements to go through each case
+  
+  if (rating === "terrible") {
+    tip = amount * 0;
+  } 
+  
+  else if (rating === "poor") {
+    tip = amount * .05;
+  }
+  
+  else if (rating === "good") {
+    tip = amount * .10;
+  }
+  
+  else if (rating === "great") {
+    tip = amount * .15;
+  }
+  
+  else if (rating === "excellent") {
+    tip = amount * .20;
+  }
+  
+// CREATE else statement to capture any null value  
+  else  {
+    return "Rating not recognized"
+  }
+  
+  tip = Math.ceil(tip);
+  return tip;
+}
