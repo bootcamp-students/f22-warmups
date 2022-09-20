1e4ba8ad091b93ae62388d50ccee0554046e1519
RDG97 <100734131+RDG97@users.noreply.github.com>
2022-09-20 08:46:05 -0400

    Create is-n-divisable-by-x-and-y.js

---
+++ gann-ryan/is-n-divisable-by-x-and-y.js
@@ -0,0 +1,9 @@
+function isDivisible(n, x, y) {
+  // check if there is a remainder in n/x and n/y
+  if (n%x === 0 && n%y === 0){
+    // return true or false
+    return true;
+  } else {
+    return false;
+}
+}
