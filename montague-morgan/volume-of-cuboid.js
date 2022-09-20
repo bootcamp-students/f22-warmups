3ad833372ee77594aa6348d01b271b1485adffb6
C. Morgan Montague <111764605+morganmontague@users.noreply.github.com>
2022-09-20 08:16:15 -0400

    Create volume-of-cuboid.js

---
+++ montague-morgan/volume-of-cuboid.js
@@ -0,0 +1,9 @@
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return (length*width*height)
+  
+  }
+}
+// need to get the volume of cuboid
+// volume of a cuboid or box is length*width*heightheight
+// return length*width*height
