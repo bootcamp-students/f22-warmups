cc061ae17c5315a07803f8f0aef4952d9e5e2a57
Jeff Goens <jeff.goens@student.awesomeinc.org>
2022-09-20 08:15:58 -0400

    Create volume-of-a-cuboid.js

---
+++ goens-jeff/volume-of-a-cuboid.js
@@ -0,0 +1,18 @@
+// Pseudocode
+
+// START
+// CREATE variable to hold volume calculation
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    let volume = length * width * height;
+    
+// TEST    
+    
+    console.log(volume, 'volume');
+    return volume;
+  
+  }
+}
+
+// END
