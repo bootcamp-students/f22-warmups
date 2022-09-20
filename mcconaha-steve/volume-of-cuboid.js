01e634da3e2713833fa5c6005efe77f1e629ce57
smcconaha <110992120+smcconaha@users.noreply.github.com>
2022-09-20 15:26:24 -0400

    Create volume-of-cuboid.js
    Adding solution to volume of cuboid

---
+++ mcconaha-steve/volume-of-cuboid.js
@@ -0,0 +1,13 @@
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      //FUNCTION(length, width, height)
+      // Determine length
+      // Determine width
+      // Determine height
+      //volumeOfCuboid = length * width * height
+      let volume = length * width * height;
+      //return volumeOfCuboid
+      return volume;
+      //ENDFUNCTION
+    }
+  }
