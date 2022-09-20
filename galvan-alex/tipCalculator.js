12232bc7f14eb2e27a1c64b08323a6e2707a4404
alexGalvan0 <98929743+alexGalvan0@users.noreply.github.com>
2022-09-19 20:35:17 -0400

    Create tipCalculator.js

---
+++ galvan-alex/tipCalculator.js
@@ -0,0 +1,33 @@
+
+// CREATE function called calculateTip that accepts two parameters. (int, string)
+function calculateTip(amount, rating){
+  // IF input not valid RETURN 
+  if(rating == undefined){
+   return 'Rating not recognised';
+  }
+  // ELSE create an object that contains type of experience as a key and the percentage of tip as value.
+  else {
+      const tipObj = {
+                  'terrible': 0,
+                  'poor': .05,
+                  'good': .1,
+                  'great': .15,
+                  'excellent': .2
+                  }
+    // Make string input all lower case to compare with object key. store into variable
+      const lowerRating = rating.toLowerCase();
+    // Create variable to hold inputed lowerCased string into object to retun the percentage associated with experience
+      const tipPercentage = tipObj[lowerRating];
+    //multiply amount times created variable store that into variable
+      const rawTip = amount * tipPercentage;
+    //rount up and store in variable
+    let roundedTip = Math.ceil(rawTip);
+    //if roundedTip is Nan return rating not recognized
+    if(isNaN(roundedTip)){
+      return 'Rating not recognised';
+    } else {
+      return roundedTip;
+    }
+  }
+}
+
