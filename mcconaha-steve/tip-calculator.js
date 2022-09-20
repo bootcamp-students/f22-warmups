570e3f3408e1712547af0aa2a3840155bb1f5d6b
smcconaha <110992120+smcconaha@users.noreply.github.com>
2022-09-19 22:25:16 -0400

    Update tip-calculator.js
    Warmup kata number 1 solution finished

--- mcconaha-steve/tip-calculator.js
+++ mcconaha-steve/tip-calculator.js
@@ -1,34 +1,39 @@
+/*  FUNCTION calculateTip
+    IF rating equals terrible
+        THEN tip equals 0%
+    ELSE IF rating equals poor
+        THEN tip equals 5%
+    ELSE IF rating equals good
+        THEN tip equals 10%
+    ELSE IF rating equals great
+        THEN tip equals 15%
+    ELSE IF rating equals excellent
+        THEN tip equals 20%
+    ELSE rating not recognised
+    
+    tipTotal equals tip times amount rounded up
+    
+    return tipTotal 
+ENDFUNCTION */
+
 function calculateTip(amount, rating) {
-    /* IF rating equals terrible
-          THEN tip equals 0%
-       ELSE IF rating equals poor
-          THEN tip equals 5%
-       ELSE IF rating equals good
-          THEN tip equals 10%
-       ELSE IF rating equals great
-          THEN tip equals 15%
-       ELSE IF rating equals excellent
-          THEN tip equals 20%
-       ELSE rating not recognised
-      tipTotal equals tip times amount
-      RETURN tipTotal
-    */
-      //IF rating equals terrible
-      let tip;
-      if (rating == "terrible") {
-        tip === 0.0;
-      } else if (rating == "poor") {
-        tip === 0.5;
-      } else if (rating == "good") {
-        tip === 0.1;
-      } else if (rating == "great") {
-        tip === 0.15;
-      } else if (rating == "excellent") {
-        tip === 0.2
-      } else {
-        return "Rating not recognized"
-      };
-      let tipTotal = tip * amount;
-      return tipTotal;
-    };
-    console.log(calculateTip(20, "eXcellent"));  
+  let tip;
+  let lowerRating = rating.toLowerCase();
+  if (lowerRating == "terrible") {
+    tip = 0;  
+  } else if (lowerRating  == "poor") {
+    tip = 0.05;
+  } else if (lowerRating  == "good") {
+    tip = 0.1;
+  } else if (lowerRating  == "great") {
+    tip = 0.15;
+  } else if (lowerRating  == "excellent") {
+    tip = 0.2
+  } else {
+    return "Rating not recognised"
+  };
+  let tipTotal = Math.ceil(tip * amount);
+  return tipTotal;
+};
+//Test
+console.log(calculateTip(10, "eXcellent"));  
