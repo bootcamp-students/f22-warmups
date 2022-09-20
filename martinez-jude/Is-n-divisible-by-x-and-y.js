4571191edb4fe4a4e498696e682d5111e9879214
SpaceBun <97587094+SpaceBunEth@users.noreply.github.com>
2022-09-20 08:25:25 -0400

    Create Is-n-divisible-by-x-and-y.js
    added kata code and javascript long version.

---
+++ martinez-jude/Is-n-divisible-by-x-and-y.js
@@ -0,0 +1,10 @@
+function isDivisible(n, x, y) {
+return (n%x == 0 && n%y == 0 ? true: false);
+}
+  
+//   if (n%x == 0 && n%y == 0){
+//     return true
+//   } else {
+//     return false
+//   }
+// }
