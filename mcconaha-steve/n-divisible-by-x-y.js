d74049d7f1087f368f40198f66a6b251261523b8
smcconaha <110992120+smcconaha@users.noreply.github.com>
2022-09-20 15:25:08 -0400

    Create n-divisible-by-x-y.js
    Adding solution to "n" divisible by "x" and "y".

---
+++ mcconaha-steve/n-divisible-by-x-y.js
@@ -0,0 +1,9 @@
+function isDivisible(n, x, y) {
+    //FUNCITON
+    console.log(n);
+    return ((n % x)==0 && (n % y)== 0);
+    //IF N is divisible by X  and N is divisible by X
+        //THEN return TRUE
+    //ELSE return FALSE
+    //ENDFUNCTION
+  }
