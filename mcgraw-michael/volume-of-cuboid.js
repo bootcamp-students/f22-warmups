b3e960ef81bcfa7a4c2701de633dddce5d819185
McTimidation <90709441+McTimidation@users.noreply.github.com>
2022-09-20 08:12:24 -0400

    Create volume-of-cuboid.js

---
+++ mcgraw-michael/volume-of-cuboid.js
@@ -0,0 +1,9 @@
+
+  
+  static getVolumeOfCuboid(length, width, height) {
+//     get volume of box
+    let volume = (length * width * height);
+//  return that volume
+    return volume;
+//     profit
+  }
