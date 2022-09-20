e35908471d0dfd3a9c79e1c0e4d58a97fe560b94
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-09-20 22:58:41 +0000

    working through logic

--- williams-mason/tip_calculator.js
+++ williams-mason/tip_calculator.js
@@ -1,2 +1,54 @@
-function calculateTip(amount, rating)
-//
+function calculateTip(amount, rating) {
+    let tipPercent;
+    //cInput equals lowercase of rating
+    let cInput = rating.toLowerCase()
+    //if cInput equals terrible
+    if (cInput === 'terrible'){
+         // tipPercent = 0.0    
+        tipPercent = 0.0;
+        //return tipPercent
+        return(tipPercent)
+    }
+    //if cInput equals poor
+    if (cInput === 'poor') {
+        // tipPercent = 0.05
+        tipPercent = 0.05;
+        //return tipPercent
+        return(tipPercent)
+
+    }
+    //if cInput equals good
+    if (cInput === 'good') {
+        // tipPercent = 0.10
+        tipPercent = 0.10;
+        //return tipPercent
+        return(tipPercent)
+
+    }
+    //if cInput equals great
+    if (cInput === 'great') {
+        // tipPercent = 0.15
+        tipPercent = 0.15;
+        //return tipPercent
+        return(tipPercent)
+
+    }
+    //if cInput equals excellent
+    if (cInput === 'excellent') {
+        // tipPercent = 0.20
+        tipPercent = 0.20
+        //return tipPercent
+        return(tipPercent)
+
+    }
+    //if anything else return 'rating not recognized'
+    else {
+        return('rating not recognized');
+    }
+
+    // tipPercent times amount = tipTotal
+    const tipTotal = tipPercent * amount;
+    //return tipTotal
+    return tipTotal;
+    
+    }
