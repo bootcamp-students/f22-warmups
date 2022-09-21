b940f0530e40a4e7c201808b6bc34d3b5d971e6e
Logan Hall <51933030+LhallLhall@users.noreply.github.com>
2022-09-21 08:24:09 -0400

    Update WillYouMakeIt.js

--- hall-logan/WillYouMakeIt.js
+++ hall-logan/WillYouMakeIt.js
@@ -1,8 +1,7 @@
 //   it needs to return a boolean (true/false)
 //   nearest pump is 50 miles (distanceToPump) away
 //   car has 25 miles per gallon(mpg)
-//   there are 2 gallons(fuelLeft) left
-//   fuelLeft * mpg = distanceToPump 
+//   there are 2 gallons(fuelLeft) left 
 //    if distanceToPump doesnt equal fuelleft * mpg return false if it does return true
 
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
