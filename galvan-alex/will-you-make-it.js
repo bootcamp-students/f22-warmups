4d169057c885ce4aab13df617d7a9620731bc55e
alexGalvan0 <98929743+alexGalvan0@users.noreply.github.com>
2022-09-21 08:23:52 -0400

    Create will-you-make-it.js

---
+++ galvan-alex/will-you-make-it.js
@@ -0,0 +1,8 @@
+// CREATE function zeroFuel that holds 3 ints
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  // TODO
+
+// if distancetopump <= distanceToPump * mpg = true
+ return distanceToPump <= (mpg*fuelLeft)  
+//else False
+}
