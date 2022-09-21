8d609977313b45b0341c26167ff6bf73ddf777b1
smcconaha <110992120+smcconaha@users.noreply.github.com>
2022-09-21 08:46:09 -0400

    Create what-is-between.js
    Added solution for "what is between" kata

---
+++ mcconaha-steve/what-is-between.js
@@ -0,0 +1,10 @@
+function between(a, b) {
+    //DEFINE array
+    let arr =[];
+    //FOR every number between a and b
+    for (i = a; i <= b; i++) {
+      arr.push(i);
+    }
+    return arr;
+    //RETURN array
+  };
