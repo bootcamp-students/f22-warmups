1b15c3cbbb1832868ce081c60994b24cc5f23a7c
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-09-21 12:15:52 +0000

    completed between kata

---
+++ williams-mason/arrayPlusArray.js
@@ -0,0 +1,18 @@
+function arrayPlusArray(arr1, arr2) {
+    //declare arr1 as an object
+    const firstArray = arr1;
+    //declare arr2 as an object
+    const secondArray = arr2;
+    //loop through the index of arr1
+    for (num of firstArray) {
+        let 
+
+    }
+        //for every intiger in index, add to the last
+        //store in a variable named?
+    //loop through the index of arr2
+        //for every intiger in index, add to the last
+        //store in a variable named?
+    //add both numbers together
+    //return sum
+  }
