36e1dc23e2519c751e72f535291b33ef0396e4a2
Josh <111532201+Joshtdale@users.noreply.github.com>
2022-09-21 08:47:49 -0400

    Create will-you-make-it.js

---
+++ will-you-make-it.js
@@ -0,0 +1,13 @@
+// Josh and Ryan
+
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  // mpg * fuelLeft
+  // if value === distance to pump return true
+  let range = mpg * fuelLeft
+  if (range === distanceToPump){
+    return true
+  } else {
+    return false
+  }
+};
