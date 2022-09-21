44e8c90fa52fae4401cb631e8c765d99082b9e70
Roger Mullins <38049025+rdmullins@users.noreply.github.com>
2022-09-21 08:56:59 -0400

    Create miles-remaining.js

---
+++ mullins-roger/miles-remaining.js
@@ -0,0 +1,14 @@
+// PSEUDOCODE
+//
+// IF distanceToPump/fuelLeft <= mpg
+// RETURN true
+// ELSE false
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  // TODO
+   return ((distanceToPump/fuelLeft) <= mpg);
+//     return true;
+//   } else {
+//     return false;
+//   }
+};
