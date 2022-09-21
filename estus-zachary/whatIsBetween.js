6af693c7b1a5366e744a0ed5c2da61e330d07c4e
Zach Estus <43345401+Zestus01@users.noreply.github.com>
2022-09-21 08:09:08 -0400

    Warmup 9/21

---
+++ estus-zachary/whatIsBetween.js
@@ -0,0 +1,9 @@
+// Create an empty array to push values to
+// Loop between each number from a to b
+
+function between(a, b) {
+  let newArr = [];
+  for(let i = a; i <= b; i++)
+    newArr.push(i);
+  return newArr;
+}
