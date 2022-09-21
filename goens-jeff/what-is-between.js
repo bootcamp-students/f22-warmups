ca0646c333484771c44d9712811b3252f385bd0c
Jeff Goens <jeff.goens@student.awesomeinc.org>
2022-09-21 08:37:05 -0400

    Create what-is-between.js

---
+++ goens-jeff/what-is-between.js
@@ -0,0 +1,13 @@
+// Pseudocode
+// CREATE blank array
+// CREATE for loop
+// PUSH to array
+
+function between(a, b) {
+  let results = [];
+  for (let i = a; i <= b; i++) {
+    results.push(i);
+    
+    }
+   return results;
+  }
