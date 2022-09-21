326a283ff9974ae95ed7bba7d77413e8f1348858
SpaceBun <97587094+SpaceBunEth@users.noreply.github.com>
2022-09-21 08:32:23 -0400

    Create What-is-between.js

---
+++ martinez-jude/What-is-between.js
@@ -0,0 +1,16 @@
+function between(a, b) {
+  let arry = [];
+  for (let i = a; i < b+1; i++ ){
+    arry.push(i);
+  }
+  console.log(arry)
+  return arry
+}
+
+// func perims a and b
+// create a empty list var
+// loop using var i as a condition 
+// increase i by + 1 every loop
+// if loop i is greater then var b+1 break loop
+// list var add the value of i to end of list
+// return list var when loop is complete
