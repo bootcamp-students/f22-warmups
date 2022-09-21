9235c3c706cedc45f40689c94cf2f9932c3397e3
SpaceBun <97587094+SpaceBunEth@users.noreply.github.com>
2022-09-21 08:35:17 -0400

    Create Will-you-make-it.js

---
+++ martinez-jude/Will-you-make-it.js
@@ -0,0 +1,16 @@
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  // TODO\
+  return (fuelLeft*mpg >= distanceToPump);
+  };
+//   if (fuelLeft*mpg >= distanceToPump){
+//     return true;
+//   } else {
+//     return false;
+//   } 
+// };
+
+// Func zeroFuel 3 perims distanceToPump, mpg, and fuelLeft
+// return short hand if statement
+// condition 
+// if true will return true 
+// if false will return false
