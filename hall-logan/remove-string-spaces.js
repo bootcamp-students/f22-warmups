fbcadd7bd895b55a27e2fbf124d393190066ae77
Logan Hall <51933030+LhallLhall@users.noreply.github.com>
2022-09-27 08:21:26 -0400

    Create remove-string-spaces.js

---
+++ hall-logan/remove-string-spaces.js
@@ -0,0 +1,11 @@
+// use a method to replace the balnk spaces with no spaces
+
+function noSpace(x){
+ 
+  let replace = x.split(" ")
+  return replace.join("")
+
+  console.log(replace)
+//   replace.join()
+
+}
