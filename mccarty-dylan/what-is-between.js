0fd743ef284c0fab8070dcb0801e5bf66a892602
DylanMcCarty <dylan.mccarty@student.awesomeinc.org>
2022-09-27 08:48:22 -0400

    Create what-is-between.js

---
+++ mccarty-dylan/what-is-between.js
@@ -0,0 +1,12 @@
+function between(a, b) {
+  let ans = []
+  for (let i = a; i <= b; ++i){
+    ans.push(i);
+    }
+  return ans
+ }
+
+//et b = greater than a 
+// set i = 1 
+//have i count up to b from a 
+//
