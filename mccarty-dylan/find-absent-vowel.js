6d841193aa9542080336079bf173da07fcff3e7b
DylanMcCarty <dylan.mccarty@student.awesomeinc.org>
2022-09-27 08:51:48 -0400

    Create find-absent-vowel.js

---
+++ mccarty-dylan/find-absent-vowel.js
@@ -0,0 +1,27 @@
+function absentVowel(x){
+  let a = 0
+  let e = 1
+  let i = 2
+  let o = 3
+  let u = 4
+  if (x.includes('a')){
+    } else {
+      return 0
+    }
+  if (x.includes('e')){
+    } else {
+      return 1
+    }
+  if (x.includes('i')){
+    } else {
+      return 2
+    }
+  if (x.includes('o')){
+    } else {
+      return 3
+    }
+  if (x.includes('u')){
+    } else {
+      return 4
+    }
+  }
