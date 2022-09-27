dda7d7505c89060f2853cc81f1e53570d0dce5f0
Logan Hall <51933030+LhallLhall@users.noreply.github.com>
2022-09-27 08:57:41 -0400

    Create absent-vowel.js

---
+++ hall-logan/absent-vowel.js
@@ -0,0 +1,30 @@
+// check string for a e i o u if it has one return it's index
+function absentVowel(x){
+console.log(x);
+  let string = x;
+  if(!string.includes("a")){
+    return 0;
+  }
+  
+  if(!string.includes("e")){
+    return 1;
+  }
+  
+  if(!string.includes("i")){
+    return 2;
+  }
+  
+  if(!string.includes("o")){
+    return 3;
+  }
+  
+  if(!string.includes("u")){
+    return 4;
+  }
+  
+    
+  
+
+    
+  }
+  
