8c71f36f61f8915cd22327fec8d9c277d8aa8d51
SpaceBun <97587094+SpaceBunEth@users.noreply.github.com>
2022-09-27 09:01:33 -0400

    Create Remove-String-Spaces.js

---
+++ martinez-jude/Remove-String-Spaces.js
@@ -0,0 +1,8 @@
+function noSpace(x){
+let y = x.split(" ").join("");
+return y;
+}
+
+// y is equal to x string apply method .split and .join
+// .split splits a string into two substrings at string value ' ' of y string
+// .join combines the two substrings with '' value inbetween
