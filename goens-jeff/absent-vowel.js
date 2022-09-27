42a1b23edb4765af0df328e9ca29b0b7dbea75cc
Jeff Goens <jeff.goens@student.awesomeinc.org>
2022-09-27 08:52:54 -0400

    Create absent-vowel.js

---
+++ goens-jeff/absent-vowel.js
@@ -0,0 +1,19 @@
+// Pseudocode
+
+// STORE the vowels in a variable
+// LOOP through the values
+// FIND the index of the vowels and substract 1 this will find the zero indexed number
+// RETURN number
+
+
+
+function absentVowel(x){
+  let vowels = "aeiou";
+  
+  for (let i in vowels) {
+    if (x.indexOf(vowels[i]) == -1) {
+      return Number(i);
+    }
+  }
+  
+  
