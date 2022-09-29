b45bf6f8cff10cd7de87db751b3fa897a7789038
Jeff Goens <jeff.goens@student.awesomeinc.org>
2022-09-29 08:21:58 -0400

    Create breaking-chocolate-problem.js

---
+++ goens-jeff/breaking-chocolate-problem.js
@@ -0,0 +1,16 @@
+// pseudocode
+// find area of chocolate bar
+// if area is greater than 0 return barSize
+// else return 0
+
+
+
+
+function breakChocolate(n,m) {
+  let barSize = (n * m) -1;
+  if (barSize >  0) {
+    return barSize;
+  } else {
+    return 0;
+  }
+ }
