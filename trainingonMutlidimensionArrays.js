802fabf9ab54d8625ea0f8b356cbf6b27446aaea
Zach Estus <43345401+Zestus01@users.noreply.github.com>
2022-09-29 08:24:27 -0400

    Uploaded to the wrong directory

--- trainingonMutlidimensionArrays.js
+++
@@ -1,9 +0,0 @@
-// loop through the values of indexes
-// This will give us the "directions" to the array we need
-// Set the array to the array at indexes[i]
-function getElement(array, indexes) {
-  for(let i of indexes){
-    array = array[i];
-  }
-  return array;
-}
