01da7879b8f5395cd4bfeae8ad7f4a0d230f7338
smcconaha <110992120+smcconaha@users.noreply.github.com>
2022-10-02 14:52:05 -0400

    Update breaking-chocoloate.js

--- mcconaha-steve/breaking-chocoloate.js
+++ mcconaha-steve/breaking-chocoloate.js
@@ -1,10 +1,10 @@
-//while n > 0, m > 0, and n + m > 2
+//while n > 0, m > 0
 //split chocolate bar length x width into squares
 //square is 1x1
 //return minimum number of breaks needed
 
 function breakChocolate(n,m) {
-    if (((n === 0) || (m === 0)) || (n +m < 2))  {
+    if (((n === 0) || (m === 0)) {
       return 0;
     } else {
       return (n * m)-1;
