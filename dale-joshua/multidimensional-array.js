6b74e1b068bf2055ea2c1471010c840825a9a187
Josh <111532201+Joshtdale@users.noreply.github.com>
2022-10-02 14:13:45 -0400

    Create multidimensional-array.js

---
+++ dale-joshua/multidimensional-array.js
@@ -0,0 +1,18 @@
+function getElement(array, indexes) {
+//   console.log(array.indexes[0])
+//   let x = indexes[0]
+//   let y = indexes[1]
+//   console.log(indexes)
+//   console.log(array)
+  
+//     let i = 0
+ for (let i = 0; i < indexes.length; i++) {
+    array = array[indexes[i]];
+  } return array
+//     array = array[i]
+//   console.log(array[x][y])
+//     console.log(i)
+//   console.log(array[0][0])
+//   let newArray = array[x][y]
+//   return array[x][y]
+}
