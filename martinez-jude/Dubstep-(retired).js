6b9ed1b4f585762073145e114560de3cab190274
SpaceBun <97587094+SpaceBunEth@users.noreply.github.com>
2022-10-06 09:17:21 -0400

    Create Dubstep-(retired).js

---
+++ martinez-jude/Dubstep-(retired).js
@@ -0,0 +1,24 @@
+function songDecoder(song){
+  let array = song.split('WUB')
+  let array1 = array.filter((arr) => arr !== '')
+  return array1.join(' ')
+}
+
+
+
+
+// loop through array
+// find the start of 'WUB'
+// find the end of 'WUB'
+    
+ 
+  
+//   const array = song.split('WUB')
+//   let string = ''
+//   for(const i of array){
+//     string = string + " " + i;
+//   }
+//   console.log(string)
+//   return string
+  
+  
