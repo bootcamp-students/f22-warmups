f578101e3b57211f09a51010247b283a5919574f
SpaceBun <97587094+SpaceBunEth@users.noreply.github.com>
2022-10-08 14:31:01 -0400

    Update Make-every-after-X-into-caps.js

--- martinez-jude/Make-every-after-X-into-caps.js
+++ martinez-jude/Make-every-after-X-into-caps.js
@@ -1,28 +1,29 @@
 var makeEveryLetterAfterXCaps = function (str, letter) {
-
-  
-  let newString = 1 + (str.indexOf(letter));
-  console.log(newString);
-  newString = str.charAt(newString);
-  console.log(newString)
-  let insertString = newString.toUpperCase(newString);
-  console.log(insertString)
-  
-  let x = str.charAt(newString)
-  console.log("testing ",x)
-  let y = str.split(newString).join(insertString);
-  
-  console.log(y)
-  
-  
-  return y
-  
-  
+let newString = ''
+for(let i = 0; i < str.length; i++){
+  if(str[i] == letter && i != (str.length -1)){
+    newString = newString + str[i]
+    i++
+    newString = newString + str[i].toUpperCase()
+  } else{
+    newString = newString + str[i]
+  }
 }
-//.join(letter.toUpperCase(letter));
-// str var indexOf(letter)
-// returns a int + 1
-// set indexDo var to = str.charAt(int + 1)
-//  cap string
+return newString
 
-// const new string = str.split(str).join(indexDo)
+}
+// Not successful
+//START 
+//FUNC capLetter INPUT 2 Params (str, letter)
+// SET newString = ''
+// FOR condition (SET i = 0; i < length of string int; i = i + 1 every loop)
+// IF str[i] is equal to letter AND i is NOT equal to (length of str - 1)
+// newString = newString + str[i]
+// i = i + 1
+// newString = newString + str[i].(HandWave method string to uppercase)
+// ELSE
+// newString = newString + str[i]
+// END IF
+// END FOR
+//RETURN newString
+// END FUNC
