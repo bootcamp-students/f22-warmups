1e932925b3d490bc7dccdc7e7f180f7a611dc4f5
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-10 20:39:57 +0000

    boom

---
+++ williams-mason/yourOrderPlease.js
@@ -0,0 +1,21 @@
+function order(words){
+    let handwave = words.split(' ')
+    let num = '';
+    let newArray = []
+    
+    for(let i = 0; i < handwave.length; i++){
+      newArray.push('')
+    }
+    for(let i = 0; i < handwave.length; i++){
+      let str = handwave[i]
+      for(let j = 0; j < str.length; j++){
+        if(isNaN(str[j]) == false){
+          num = str[j]
+          num = parseInt(num)
+          num--
+          newArray.splice(num,1,handwave[i])
+        }
+      }
+    }
+    return newArray.join(' ')
+  }
