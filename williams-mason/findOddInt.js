02b4aea7be1de33da2b534b34dcc29860484d454
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-10 12:44:09 +0000

    done baby

---
+++ williams-mason/findOddInt.js
@@ -0,0 +1,14 @@
+function findOdd(A) {
+    let num = A.length;
+      for (let i = 0; i < num; i++) {
+        let count = 0;
+          for (let j = 0; j < num; j++) {
+            if (A[i] == A[j]) {
+              count = count + 1;
+            }
+          }
+        if (count %2 !==0) {
+          return A[i];
+        }
+      }
+    }
