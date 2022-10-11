190147265e1fc10c3fc56ecf24f2f63a9c74c335
SpaceBun <97587094+SpaceBunEth@users.noreply.github.com>
2022-10-11 07:51:27 -0400

    Update Find-the-odd-int.js

--- martinez-jude/Find-the-odd-int.js
+++ martinez-jude/Find-the-odd-int.js
@@ -13,3 +13,16 @@ function findOdd(A) {
     }
   }
 }
+
+//START
+// FUNC findOdd() PARAM(A) - an array of numbers
+// SET num = A.length
+// FOR Condition (SET i = 0; i < num; i++
+// SET count = 0
+// FOR Condition (SET j = 0; j < num; j++)
+// IF (A[i] == A[j])
+// count++
+// FOR END
+// IF (count % 2 !== 0)
+// RETURN A[i]
+// FOR END
