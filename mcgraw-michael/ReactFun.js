3fb39d5ee47b93627229fd7f92601dfdffc44ccc
McTimidation <90709441+McTimidation@users.noreply.github.com>
2022-10-12 08:37:19 -0400

    Create ReactFun.js

---
+++ mcgraw-michael/ReactFun.js
@@ -0,0 +1,22 @@
+
+
+function Hello(){
+  return (
+    <h1>Hello</h1>
+    )
+} 
+function World(){
+  return ( <h2>World!</h2>
+    )
+}
+
+class Greet extends React.Component {
+  render() {
+    return (
+    <div>
+    <Hello />
+    <World />
+    </div>
+    )
+  }
+}
