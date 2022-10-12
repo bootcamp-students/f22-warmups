5ace8a39132d70e865297b3e3f06e97a844c716c
McTimidation <90709441+McTimidation@users.noreply.github.com>
2022-10-12 08:24:39 -0400

    Create createphonenumber.js

---
+++ mcgraw-michael/createphonenumber.js
@@ -0,0 +1,3 @@
+function createPhoneNumber(numbers){
+  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
