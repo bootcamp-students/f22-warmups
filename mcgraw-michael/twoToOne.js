07126763a49a28b6367b1b32964079e16df72653
McTimidation <90709441+McTimidation@users.noreply.github.com>
2022-10-12 09:10:03 -0400

    Create twoToOne.js

---
+++ mcgraw-michael/twoToOne.js
@@ -0,0 +1,17 @@
+function longest(s1, s2) {
+    // split strings into 1 array
+    let str = s1+s2;
+     let array1 = str.split('');
+    let array2 = [];
+  //  remove duplicates
+    array1.forEach((char) => {
+      if (!array2.includes(char)) {
+        array2.push(char);
+      }
+    })
+    // sorts alphabetically then converts back into a string
+    console.log(array2);
+    array2 = array2.sort();
+    console.log(array2);
+    return array2.join('');
+  }
