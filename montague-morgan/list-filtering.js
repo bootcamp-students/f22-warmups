6f67008d378de8e0f1e9214c8c7aae8dd6da775e
C. Morgan Montague <111764605+morganmontague@users.noreply.github.com>
2022-10-16 17:52:55 -0400

    Create list-filtering.js
    added to wrong area

---
+++ montague-morgan/list-filtering.js
@@ -0,0 +1,9 @@
+function filter_list(l) {
+  // Return a new array with the strings filtred out
+ return l.filter(list => typeof list == "number");
+}
+// function isNumber
+
+// given an array with strings and intergers (non negative)
+// need to extract the intergers from the array and return the a new array with just the number
+// using filter, we will check to see if the array value === number and return just those
