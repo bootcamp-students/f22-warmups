2c36ffe3ebdbdd079478ebb07325b7a198666af1
C. Morgan Montague <111764605+morganmontague@users.noreply.github.com>
2022-10-18 20:20:28 -0400

    Update backwords.js

--- montague-morgan/backwords.js
+++ montague-morgan/backwords.js
@@ -1,9 +1,9 @@
 function spinWords(string){
-    let HI_KEITH_HOPE_YOUR_DAY_IS_MEH = [];
+    let HI_KEITH_HOPE_YOUR_DAY_IS_GREAT = [];
     let words = string.split(' ');
     words = words.forEach(word => {
       if(word.length <=4){
-        HI_KEITH_HOPE_YOUR_DAY_IS_MEH.push(word);
+        HI_KEITH_HOPE_YOUR_DAY_IS_GREAT.push(word);
       }
       if(word.length >= 5){
         let letters = word.split('');
@@ -11,10 +11,10 @@ function spinWords(string){
         console.log(letters);
         letters = letters.join('');
         console.log('afterJoin', letters);
-        HI_KEITH_HOPE_YOUR_DAY_IS_MEH.push(letters);
+        HI_KEITH_HOPE_YOUR_DAY_IS_GREAT.push(letters);
         return;
       }
     });
-    console.log(HI_KEITH_HOPE_YOUR_DAY_IS_MEH);
-    HI_KEITH_HOPE_YOUR_DAY_IS_MEH = HI_KEITH_HOPE_YOUR_DAY_IS_MEH.join(' ');
-    return (HI_KEITH_HOPE_YOUR_DAY_IS_MEH);
+    console.log(HI_KEITH_HOPE_YOUR_DAY_IS_GREAT);
+    HI_KEITH_HOPE_YOUR_DAY_IS_MEH = HI_KEITH_HOPE_YOUR_DAY_IS_GREAT.join(' ');
+    return (HI_KEITH_HOPE_YOUR_DAY_IS_GREAT);
