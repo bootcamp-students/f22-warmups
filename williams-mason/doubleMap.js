cdb3101e16b6e47d30e310582575a9180ef4993f
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-10-20 12:10:21 +0000

    boom

---
+++ williams-mason/doubleMap.js
@@ -0,0 +1,6 @@
+function double(array) {
+    let newArray= array.map(i=>i*2);
+    return (newArray);
+      // Use array.map() to return a new array with each value twice
+      // as large as the corresponding value in the passed in array.
+  }
