8959feee96e282e90132df00c60c15f6feb94352
Tyler Consalvi <106493956+tkcthulhu@users.noreply.github.com>
2022-10-24 09:04:16 -0400

    Create unique-number.js

---
+++ consalvi-tyler/unique-number.js
@@ -0,0 +1,16 @@
+// sort through the array so that the number is either the first index or the last
+// loop through the array
+// if first index is less than the second return the first index
+// if the last index is greater than last index minus 1
+// if it is than return the last index
+
+function findUniq(arr) {
+  arr = arr.sort()
+  let max = arr.length - 1
+  console.log(arr[max])
+  if (arr[0] !== arr[1]) {
+    return arr[0] 
+  } else if (arr[max] !== arr[max - 1]) {
+    return arr[max]
+  }
+}
