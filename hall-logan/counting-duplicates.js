9421fd0dd14542dc3c0c46ae27bdb727203c1971
Logan Hall <51933030+LhallLhall@users.noreply.github.com>
2022-10-25 08:02:38 -0400

    Create counting-duplicates.js

---
+++ hall-logan/counting-duplicates.js
@@ -0,0 +1,20 @@
+/*
+take the string and all to lowercase, split into an array, sort the order
+set iterable for count
+*/
+
+function duplicateCount(text){
+  //...
+  text = text.toLowerCase().split('').sort()
+  let count = 0
+  let duplicates = 0
+  for (let i =0; i< text.length; i++) {
+    if (text[i] === text[i + 1]){
+      count++
+    } else if ((count > 0)&&(text[i] !== text[i+1])){
+      duplicates++
+      count = 0
+    }
+  }
+  return duplicates
+}
