7b28a09530f5757bf216bf797ef62ff4474bb1ad
andrews-zack <111662798+andrews-zack@users.noreply.github.com>
2022-10-25 09:16:09 -0400

    Create stupid-pig-latin.js

---
+++ andrews-zack/stupid-pig-latin.js
@@ -0,0 +1,21 @@
+// split the string so each word is an index in an array
+// 
+
+
+function pigIt(str){
+  let splitted = str.split(' ')
+  let firstLetter = ''
+  let name = []
+  splitted.forEach((item) => {
+    if ((item === '!') || (item === '?')){
+      firstLetter = item.slice(0, 1)
+     let hey = item.slice(1)
+     name.push(hey + firstLetter)
+    } else {
+      firstLetter = item.slice(0, 1)
+      let hey = item.slice(1)
+      name.push(hey + firstLetter + 'ay')
+    }
+  })
+  return name.join(' ')
+}
