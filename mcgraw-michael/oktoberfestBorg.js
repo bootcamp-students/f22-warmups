2e9b7903c7888708f69fe0e5fb76408d2bb239ca
McTimidation <90709441+McTimidation@users.noreply.github.com>
2022-10-29 09:59:58 -0400

    Create oktoberfestBorg.js

---
+++ mcgraw-michael/oktoberfestBorg.js
@@ -0,0 +1,8 @@
+function gameOfFives(bavarianBeerBears,scandinavianSchöllers){
+ if (bavarianBeerBears.filter(x=> x == 5).length === scandinavianSchöllers.filter(x=> x == 5).length) {
+   return "Drinks All Round! Free Beers on Bjorg!"
+ } else {
+   return "Uh Oh! Bjorg's a donut! No beer for anyone!" 
+ }
+
+}
