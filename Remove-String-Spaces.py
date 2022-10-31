a120f79e7799f1856dc9009eb0afb99a675c94de
naodmekonnen <100172439+naodmekonnen@users.noreply.github.com>
2022-10-31 08:13:33 -0400

    Create Remove-String-Spaces.py

---
+++ Remove-String-Spaces.py
@@ -0,0 +1,9 @@
+# set a a new variable for the new string
+# use .replace(" ", "")
+# return new variable
+
+
+def no_space(x):
+    print(x)
+    replaced = x.replace(" ", "")
+    return replaced;
