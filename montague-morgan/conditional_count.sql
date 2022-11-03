7c73f4e7a081389582e892bd9a3d37942ddf6807
C. Morgan Montague <111764605+morganmontague@users.noreply.github.com>
2022-11-03 09:05:35 -0400

    Create conditional_count.sql

---
+++ montague-morgan/conditional_count.sql
@@ -0,0 +1,27 @@
+-- Replace with your SQL Query
+-- month, total_count, total_amount, mike_count, mike_amount, jon_count, jon_amount
+
+SELECT 
+  EXTRACT(MONTH FROM payment_date) AS month,
+  COUNT(*) AS total_count,
+  SUM(amount) AS total_amount,
+  SUM(CASE
+           WHEN staff_id = 1 THEN 1
+	      END
+	) AS mike_count,
+    SUM(CASE
+           WHEN staff_id = 1 THEN (amount)
+	      END
+	) AS mike_amount,
+    SUM(CASE
+           WHEN staff_id = 2 THEN 1
+	      END
+	) AS jon_count,
+    SUM(CASE
+           WHEN staff_id = 2 THEN (amount)
+	      END
+	) AS jon_amount
+  
+FROM payment
+GROUP BY month
+ORDER BY month
