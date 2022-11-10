07cbbc03bcc567a97f85fad7450055952d6caa3d
C. Morgan Montague <111764605+morganmontague@users.noreply.github.com>
2022-11-10 08:31:20 -0500

    Create rectangle_into_squares.py

---
+++ montague-morgan/rectangle_into_squares.py
@@ -0,0 +1,17 @@
+def sq_in_rect(lng, wdth):
+    if lng == wdth:
+        return None
+    
+    result = []
+
+    while lng != wdth:
+        if lng > wdth:
+            lng -= wdth
+            result.append(wdth)
+        elif wdth > lng:
+            wdth -= lng
+            result.append(lng)
+
+    result.append(lng)
+    
+    return result
