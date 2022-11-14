7d74f3454dde2857d4c4a81008a095679506dc7a
Tyler Consalvi <106493956+tkcthulhu@users.noreply.github.com>
2022-11-14 08:52:19 -0500

    Create rot13.py

---
+++ consalvi-tyler/rot13.py
@@ -0,0 +1,5 @@
+def rot13(m):
+    rot13 = str.maketrans(
+    ‘ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz’,
+    ‘NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm’)
+    return m.translate(rot13)
